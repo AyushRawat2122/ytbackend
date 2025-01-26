ba4b251dd129b316ec0a3cc12789612901aeff90
AyushRawat2122 <ayushprovalorant@gmail.com>
2025-01-26 12:00:23 +0530

    video and user model + A & R Tokens + agg pipelines plugins to video schema + pass encyrpt

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new Schema(
+  {
+    VideoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: User,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(aggregatePaginate)
+
+const Video = mongoose.model("Video", videoSchema);
+export default Video;
