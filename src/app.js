8c55c40ad36304ca6f6d2ae1abbc3063df56e343
AyushRawat2122 <ayushprovalorant@gmail.com>
2025-01-30 22:35:22 +0530

    added refresh token endpt

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
